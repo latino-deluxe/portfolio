3e999ba36ae6f24f99dd7e5d3f08cc9bb8605dd8
Emanuele Latino <emanuelelatino2206@gmail.com>
2023-01-28 16:36:19 +0100

    first version of portfolio

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio.Pages
+{
+    public partial class Index
+    {
+        public bool[] _toggleContent = new bool[2];
+
+        protected override void OnInitialized()
+        {
+            _toggleContent[0] = true;
+            base.OnInitialized();
+        }
+
+        public void ToggleContent(int id)
+        {
+            for (int i = 0; i < _toggleContent.Length; i++)
+            {
+                _toggleContent[i] = i == id ? true : false;
+            }
+        }
+    }
+}
--- obj/Debug/netstandard2.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,63 +13,63 @@ namespace portfolio.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 1 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 2 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 3 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 4 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 5 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 6 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 7 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 8 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using portfolio;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\_Imports.razor"
+#line 9 "C:\Users\manu0\Documents\latino.portfolio\portfolio\_Imports.razor"
 using portfolio.Shared;
 
 #line default
@@ -83,11 +83,11 @@ using portfolio.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\manu0\Documents\latino.portfolio\portfolio\portfolio\Shared\NavMenu.razor"
+#line 14 "C:\Users\manu0\Documents\latino.portfolio\portfolio\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+    private string NavMenuCssClass => collapseNavMenu ? "d-none" : null;
 
     private void ToggleNavMenu()
     {
